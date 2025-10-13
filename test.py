591b1450f95749de1a7a7b42da51b58acef8d05b
zacklow28 <zo.ngyun@hotmail.com>
2025-10-13 21:39:24 +0800

    Add sample UDP server code

--- test.py
+++ test.py
@@ -4,3 +4,20 @@
 # updates, unreliable but low-latency for movement or voice chat packets.
 
 # Packet header: | ChannelType (1 B) | SeqNo (2 B) | Timestamp (4 B) | Payload |.
+
+import sys
+import socket
+
+def server():
+    sPort = int(sys.argv[1])  # 2nd command line argument is your port num
+    sSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sSocket.bind(("", sPort)) 
+
+    while True:
+        msg, addr = sSocket.recvfrom(1024)
+        print(f"Received message: {msg.decode()} from {addr}")
+        if not msg:
+            break
+    sSocket.close()
+
+server()
